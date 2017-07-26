ae33a93b16375c5cc0c50066723a56ef902fa902
Unknown <d.darienco@gmail.com>
2017-07-26 18:12:11 -0300

    Collisions with igredients working

---
+++ DellaMunchies/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+
+    Vector3 dist;
+    float posX;
+    float posY;
+
+    bool isTimingOut;
+
+    void Start()
+    {
+        isTimingOut = false;
+
+    }
+
+    void OnMouseDown()
+    {
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curPos =
+            new Vector3(Input.mousePosition.x - posX,
+                Input.mousePosition.y - posY, dist.z);
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+        transform.position = worldPos;
+    }
+
+
+
+
+
+}
